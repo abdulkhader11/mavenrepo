3f79376803dd9cdaeb42028f24f037632e76501b
Abdul Khader <Abdul Khader@Abdul>
2018-09-06 11:49:41 +0530

    jenkins

---
+++ sel1/src/test/java/sel1/Mavenclas.java
@@ -0,0 +1,17 @@
+package sel1;
+
+import org.testng.annotations.Test;
+
+public class Mavenclas {
+		// TODO Auto-generated method stub
+
+		@Test
+		public void login() {
+		System.out.println("TestNG");
+
+		}
+	}
+
+	
+	
+
